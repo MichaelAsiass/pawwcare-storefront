61a8178260e386806e30d26e8fe9ddc5fc67e24e
michael.asiass@hotmail.com <michael.asiass@hotmail.com>
2025-10-08 11:49:11 -0400

    Initial commit

--- src/components/hooks/use-toast.ts
+++ src/components/hooks/use-toast.ts
@@ -1,8 +1,6 @@
 'use client'
 
-// Inspired by react-hot-toast library
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
 import type { ToastActionElement, ToastProps } from '@/components/ui/toast'
 
 const TOAST_LIMIT = 1
@@ -169,9 +167,9 @@ function toast({ ...props }: Toast) {
 }
 
 function useToast() {
-  const [state, setState] = React.useState<State>(memoryState)
+  const [state, setState] = useState<State>(memoryState)
 
-  React.useEffect(() => {
+  useEffect(() => {
     listeners.push(setState)
     return () => {
       const index = listeners.indexOf(setState)
